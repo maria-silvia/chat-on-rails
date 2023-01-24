223d381013da8746db9b4c3851fb53b8a013b4a3
Maria <mariasrr97@gmail.com>
2023-01-24 10:58:35 -0300

    fix the css

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-turbo-frame {
-	display: block;
-	border: 2px doted pink;
-}
